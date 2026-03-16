d49954755114f38832d6ba2654e2f14f9b4d2cb8
VictorManzini <vikmanzini@icloud.com>
2026-03-16 10:06:59 -0300

    Aula 2

--- analise_dos_motores.py
+++ analise_dos_motores.py
@@ -1,18 +1,22 @@
-#Aula 16/03 Demanda, potência e Fator de Potência 
+#Aula 16/03 Exercicios: Calcule aas potências ativa, aparente e reativa para os motores das especificações
+# disponíveis na pasta da aula
 #Inserção de dados pelo usuário
 
 #Valores que serão inseridos: PU ----> Potência Útil, em Watts 
 #n ----> rendimento ---> n = PU / P 
 #FP ----> fator de potência ou cosPhi ---->fp = P / S 
 
-# PU, n, FP
-# 3000, 0.76, 0.8
+# PU, n, FP 
+#Você encontra os dados na placa do motor. PU está em Kw, n está em rend. FP está em F.P
+#Motor 1: PU = 750 W, n = 83.0, FP = 0.82
+#Motor 2: PU = 7500 W, n = 91.0, FP = 0.82
+#Motor 3: PU = 1260 W, n = 0.85, FP = 0.89
+#Motor 4: PU = 30000 W, n = 95.8, FP = 0.89
 
 #Entrada de dados 
 PU = int(input ("Insira a potência útil do motor: "))
 n = float(input("Insira o valor do rendimento (de 0 a 1): "))
 FP = float(input("Insira o Fator de Potência (de 0 a 1):"))
-
 
 # Cálculo das potências ativa (P), aparente (S) e reativa (Q)
 # n = PU / P 
@@ -32,5 +36,6 @@ print(f"A potência reativa do mtor é: {int(Q)} VAr")
 
 #Exemplo com casas decimais: 
 #print(f"A potência ativa do motor é: {P: .2f} W")
+
 
 
